884117bd9d9e4937a3c4dde38570cf3b1691ca27
rw@kanpla.dk <rw@kanpla.dk>
2022-10-26 17:17:42 +0200

    fix(percy): added all in one promise

--- src/awaitVercelDeployment.ts
+++ src/awaitVercelDeployment.ts
@@ -1,10 +1,14 @@
 import * as core from '@actions/core';
+import axios from 'axios';
 import fetch from 'node-fetch';
+import querystring from 'querystring';
 import { VERCEL_BASE_API_ENDPOINT } from './config';
 import { VercelDeployment } from './types/VercelDeployment';
-import { getPreviewUrl } from './getPreviewUrl';
 
-function delay(ms) {
+const apiUrl = 'https://api.vercel.com';
+const deploymentsUrl = '/v5/now/deployments';
+
+function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
@@ -20,7 +24,52 @@ const awaitVercelDeployment = (timeout: number): Promise<VercelDeployment> => {
     let deployment: VercelDeployment = {};
     const timeoutTime = new Date().getTime() + timeout;
 
-    const baseUrl = await getPreviewUrl();
+    const token = process.env.VERCEL_TOKEN;
+    const githubProject = process.env.GITHUB_REPOSITORY;
+    const branch = process.env.BRANCH_NAME;
+    const repo = githubProject?.split('/')[1];
+    const teamId = process.env.VERCEL_TEAM_ID;
+    const projectId = process.env.VERCEL_PROJECT_ID;
+
+    const query = {
+      teamId,
+      projectId,
+    };
+    const qs = querystring.stringify(query);
+
+    core.info(`Fetching from: ${apiUrl}${deploymentsUrl}?${qs}`);
+    const { data } = await axios.get(`${apiUrl}${deploymentsUrl}?${qs}`, {
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    });
+
+    core.debug(`token: ${token}`);
+    core.debug(`branch: ${branch}`);
+    core.debug(`repo: ${repo}`);
+
+    if (!data || !data.deployments || data.deployments.length <= 0) {
+      core.error(JSON.stringify(data, null, 2));
+      throw new Error('no deployments found');
+    }
+
+    core.debug(`Found ${data.deployments.length} deployments`);
+    core.debug(`Looking for matching deployments ${repo}/${branch}`);
+    const builds = data.deployments.filter((deployment: VercelDeployment) => {
+      return deployment?.meta?.githubCommitRepo === repo && deployment?.meta?.githubCommitRef === branch;
+    });
+
+    core.debug(`Found ${builds.length} matching builds`);
+    if (!builds || builds.length <= 0) {
+      core.error(JSON.stringify(builds, null, 2));
+      throw new Error('no deployments found');
+    }
+
+    const build = builds[0];
+    core.info(`Preview URL: https://${build.url} (${build.state})`);
+
+    const baseUrl = build.url;
     core.debug(`Url to wait for: ${baseUrl}`); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true https://github.com/actions/toolkit/blob/master/docs/action-debugging.md#how-to-access-step-debug-logs
 
     while (new Date().getTime() < timeoutTime) {
